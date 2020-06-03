f5294da66bb1ce146eead6a04849a60a2489c0a2
Alican Uver <can.uverr@gmail.com>
2020-06-03 13:34:44 +0300

    CartList and CartTotal comp written

--- src/components/CartPage/CartList.js
+++ src/components/CartPage/CartList.js
@@ -1,13 +1,43 @@
-import React from 'react';
-import CartItem from './CartItem';
+import React from "react";
+import { ProductConsumer } from "../../context";
+import CartItem from "./CartItem";
 
 const CartList = () => {
-    return (
-        <div>
-            CartList
-            <CartItem />
+  return (
+    <div className="container-fluid">
+      <div className="row">
+        <div className="col">
+          <ProductConsumer>
+            {(value) => {
+              const { cart, increment, decrement, removeItem } = value;
+              if (cart.length === 0) {
+                return (
+                  <h2 className="text-uppercase text-center">
+                    cour cart is empty
+                  </h2>
+                );
+              }
+              return (
+                <React.Fragment>
+                  {cart.map((item) => {
+                    return (
+                      <CartItem
+                        key={item.id}
+                        cartItem={item}
+                        increment={increment}
+                        decrement={decrement}
+                        removeItem={removeItem}
+                      />
+                    );
+                  })}
+                </React.Fragment>
+              );
+            }}
+          </ProductConsumer>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
 export default CartList;
--- src/components/CartPage/CartTotals.js
+++ src/components/CartPage/CartTotals.js
@@ -1,11 +1,30 @@
-import React from 'react'
+import React from "react";
+import { ProductConsumer } from "../../context";
 
 const CartTotals = () => {
-    return (
-        <div>
-            Cart Totals
-        </div>
-    )
-}
+  return (
+    <div className="container">
+      <div className="row">
+        <ProductConsumer>
+          {(value) => {
+            const { clearCart, cartSubTotal, cartTax, cartTotal } = value;
+            return (
+              <div className="col text-center my-4 ">
+                <button className="btn btn-outline-danger text-capitalize mb-4"
+                onClick = {clearCart}
+                >
+                Clear Cart
+                </button>
+                <h3>Subtotal: ${cartSubTotal} </h3>
+                <h3>CartTax: ${cartTax} </h3>
+                <h3>CartTotal: ${cartTotal} </h3>
+              </div>
+            );
+          }}
+        </ProductConsumer>
+      </div>
+    </div>
+  );
+};
 
-export default CartTotals
+export default CartTotals;
